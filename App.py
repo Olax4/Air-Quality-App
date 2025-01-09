57cb175a365cf1c9515ab07fc99420030cc4dd98
AleksandraPrzybylo <aleksandraxprzybylo@gmail.com>
2025-01-09 22:59:41 +0100

    Dodanie adresów i klucza Api
    Sam klucz api google maps będzie dodany w innym miejscu dla bezpieczeństwa
    
    Co-Authored-By: Kornelia00000 <75389761+Kornelia00000@users.noreply.github.com>

--- App.py
+++ App.py
@@ -6,3 +6,11 @@ import time
 from datetime import datetime
 
 app = Flask(__name__)
+
+# Adresy i klucze
+STATIONS_URL = "https://api.gios.gov.pl/pjp-api/rest/station/findAll"
+SENSORS_URL = "https://api.gios.gov.pl/pjp-api/rest/station/sensors/"
+DATA_URL = "https://api.gios.gov.pl/pjp-api/rest/data/getData/"
+GEOCODING_URL = "https://maps.googleapis.com/maps/api/geocode/json"
+
+API_KEY = "xxxxx"  # <--  klucz do Google Maps
